3cdc3fb1cdde7258f0574690a94d54541974a8f6
fidGET01 <nazar.nk007@gmail.com>
2023-04-22 17:20:37 +0200

    Add hash password

---
+++ database.js
@@ -0,0 +1,109 @@
+const dbfile = "./chat.db"
+const dbwrapper = require("sqlite");
+const fs = require("fs")
+const exists = fs.existsSync(dbfile);
+const sqlite3 = require("sqlite3").verbose();
+let db;
+const crypto = require("crypto")
+
+dbwrapper.open({
+    filename: dbfile,
+    driver: sqlite3.Database
+}).then(async item => {
+    db = item;
+    try {
+        if(!exists){
+            await db.run(
+                `CREATE TABLE user(
+                    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    login TEXT, 
+                    password TEXT,
+                    salt TEXT
+                );`
+            )
+
+            await db.run(
+                `CREATE TABLE message(
+                    msg_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    content TEXT,
+                    author_id INTEGER,
+                    FOREIGN KEY(author_id) REFERENCES user(id)
+                 )`
+            )
+        } else{
+            console.log(await db.all("SELECT* FROM user"))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+module.exports = {
+    getMessages: async () =>{
+        try {
+            return await db.all(
+                `SELECT msg_id, content, user_id, login FROM message
+                JOIN user ON message.author_id = user.user_id`
+            )
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    addMessage: async (user_id, msg) => {
+       try {
+         await db.run(
+             `INSERT INTO message(content, author_id) VALUES (?, ?)`, [msg, user_id]
+         ) 
+       } catch (error) {
+        console.log(error)
+       }
+    },
+
+    isUserExist: async (login) => {
+        try {
+            const user = await db.all("SELECT * FROM user WHERE login = ?", [login]);
+            return user.length;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    addUser: async (user) => {
+        const salt = crypto.randomBytes(16).toString("hex");
+        const password = crypto.pbkdf2Sync(
+            user.password,
+            salt,
+            1000,
+            64,
+            'sha512' 
+            ).toString("hex")
+        try {
+            await db.run(
+                "INSERT INTO user(login, password, salt) VALUES (?, ?, ?)",
+                [user.login, password, salt]
+            )
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    getAuthToken: async(user) =>{
+        const candidate = await db.all(
+            "SELECT * FROM user WHERE login = ?",
+            [user.login]
+        );
+        if(!candidate.length){
+            throw "Wrong login"
+        }
+        const {user_id, login, password, salt} = candidate[0];
+        const hash = crypto.pbkdf2Sync(
+            user.password, salt, 1000, 64, 'sha512'
+        ).toString("hex")
+        if (password !== hash){
+            throw "Wrong password"
+        }
+        return candidate[0].user_id + "."
+        +candidate[0].login + "."
+        +crypto.randomBytes(20).toString("hex");
+    }
+}
---
+++ server.js
@@ -0,0 +1,133 @@
+const fs = require("fs")
+const path = require("path")
+const http = require("http")
+const db = require("./database")
+const cookie = require("cookie");
+
+let validateAuthToken = [];
+
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, "static", "index.html"));
+const styleCSSFile = fs.readFileSync(path.join(__dirname, "static", "style.css"));
+const scriptJSFile = fs.readFileSync(path.join(__dirname, "static", "script.js"));
+const registerHtmlFile = fs.readFileSync(path.join(__dirname, "static", "register.html"));
+const authJSFile = fs.readFileSync(path.join(__dirname, "static", "auth.js"));
+const loginHtmlFile = fs.readFileSync(path.join(__dirname, "static", "login.html"));
+const server = http.createServer((req, res) =>{
+    if(req.method == "GET"){
+        switch(req.url){
+            case "/style.css": return res.end(styleCSSFile);
+            case "/register": return res.end(registerHtmlFile);
+            case "/login": return res.end(loginHtmlFile);
+            case "/auth.js": return res.end(authJSFile);
+            default: return guarded(req, res);
+        }
+    
+    }
+    
+    if(req.method == "POST"){
+        switch(req.url){
+            case "/api/register":
+                return registerUser(req, res);
+            case "/api/login":
+                return loginUser(req, res);
+        }
+    }
+    res.statusCode == 404;
+    return res.end("Error 404");
+})
+
+server.listen(3000);
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.use((socket, next) =>{
+    const cookie = socket.handshake.auth.cookie;
+    const credentials = getCredentials(cookie);
+
+    if(!credentials){
+        next(new Error("no auth"));
+    }
+    socket.credentials = credentials;
+    next();
+});
+
+io.on("connection", async (socket) => {
+    console.log("a user connected. Id: "+socket.id);
+    let nickname = socket.credentials?.username;
+    let userID = socket.credentials?.user_id
+    let messages = await db.getMessages(); 
+    socket.emit("all_messages", messages);
+
+    socket.on("new_message", (message) => {
+        db.addMessage(userID, message);
+        io.emit("message",nickname + ": " + message);
+    })
+})    
+function registerUser(req, res){
+    let data = "";
+    req.on("data", (chunk) => {
+        data += chunk;
+    })
+    req.on("end", async () => {
+        try {
+            const user = JSON.parse(data)
+            if(!user.login || !user.password) {
+                return res.end("Empty login or password");
+            }
+            if(await db.isUserExist(user.login)) {
+                return res.end("User alredy exist");
+            }
+            await db.addUser(user);
+            return res.end("Register is successful")
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+
+function loginUser(req, res) {
+    let data = "";
+    req.on("data", (chunk) => {
+        data += chunk;
+    })
+    req.on("end", async () => {
+        try {
+            const user = JSON.parse(data);
+            const token = await db.getAuthToken(user);
+            validateAuthToken.push(token);
+            res.writeHead(200);
+            res.end(token);
+        } catch (error) {
+            res.writeHead(403);
+            res.end(error);
+        }
+    })
+}
+
+function getCredentials(c = ''){
+    const cookies = cookie.parse(c);
+    const token = cookies?.token;
+
+    if(!token || !validateAuthToken.includes(token)) return null;
+
+    const [user_id, login] = token.split(".");
+    if(!user_id || !login) return null;
+    return {user_id: user_id, username: login};
+}
+
+function guarded(req, res){
+    const credentials = getCredentials(req.headers?.cookie);
+    if(!credentials){
+        res.writeHead(302, {"Location": "/register"})
+    }
+    if(req.method == "GET"){
+        switch(req.url){
+            case '/': return res.end(indexHtmlFile);
+            case '/script.js': return res.end(scriptJSFile);
+        }
+    }
+    res.statusCode == 404;
+    res.end("Error 404")
+}
